210cfc61eea4615369c5df07b7c6e2e5f9d2fc8e
coci315 <coci315@gmail.com>
2017-05-15 23:53:15 +0800

    add my-radio.vue

---
+++ src/components/base/my-radio/my-radio.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="my-radio">
+    <span class="radio-wrap" :class="{active:selectedIndex===0}" @click="selectedIndex=0">
+      <i class="icon-male"></i>
+      <span>男</span>
+    </span>
+    <span class="radio-wrap" :class="{active:selectedIndex===1}" @click="selectedIndex=1">
+      <i class="icon-female"></i>
+      <span>女</span>
+    </span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'my-radio',
+    props: {
+      selectedIndex: {
+        type: Number,
+        default: 0
+      }
+    }
+  }
+</script>
+
+<style rel="stylesheet/scss" lang="scss">
+.my-radio {
+  font-size: 0;
+  .radio-wrap {
+    display: inline-block;
+    padding-left: 27px;
+    margin-right: 12px;
+    width: 63px;
+    height: 46px;
+    line-height: 46px;
+    border: 1px solid #f1f3e9;
+    border-radius: 3px;
+    font-size: 0;
+    color: #b5b5b5;
+    background-color: #fff;
+    cursor: pointer;
+    &.active {
+      color: #333;
+      font-weight: bold;
+    }
+    &>* {
+      display: inline-block;
+      vertical-align: middle;
+    }
+    i {
+      margin-right: 10px;
+      font-size: 18px;
+    }
+    span {
+      font-size: 14px;
+    }
+  }
+}
+</style>
--- src/components/basic-info/basic-info.vue
+++ src/components/basic-info/basic-info.vue
@@ -26,19 +26,142 @@
       </div>
     </div>
     <div class="editor-part" v-show="isShowEditor">
-      <label>显示身份</label>
-      <selector></selector>
+      <div class="edit-wrap">
+        <label>显示身份</label>
+        <selector :options="optsIdentity"></selector>
+      </div>
+      <div class="edit-wrap">
+        <label>性别</label>
+        <my-radio></my-radio>
+      </div>
+      <div class="edit-wrap">
+        <label>生日</label>
+        <div class="selectors">
+          <selector :options="optsYears" :selectedIndex="selectedYearIndex" size="small" selectorClass="selector_year"></selector>
+          <selector :options="optsMonths" :selectedIndex="selectedMonthIndex" size="extraSmall"></selector>
+        </div>
+      </div>
+      <div class="edit-wrap">
+        <label>最高学历</label>
+        <selector :options="optsEducation" :selectedIndex="selectedEducationIndex"></selector>
+      </div>
+      <div class="edit-wrap">
+        <label>工作年限</label>
+        <selector :options="optsSeniority" :selectedIndex="selectedSeniorityIndex"></selector>
+      </div>
+      <div class="edit-wrap">
+        <label>所在城市</label>
+        <city-selector></city-selector>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
   import selector from '../base/selector/selector'
+  import myRadio from '../base/my-radio/my-radio'
+  import citySelector from '../base/city-selector/city-selector'
+
+  function generatorOptsYears (start, end) {
+    const optsYears = []
+    for (let i = end; i >= start; i--) {
+      optsYears.push({label: i, value: i})
+    }
+    return optsYears
+  }
+
+  function paddingLeft (num) {
+    return num < 10 ? '0' + num : '' + num
+  }
+
+  function generatorOptsMonths (start = 1, end = 12) {
+    const optsMonths = []
+    for (let i = start; i <= end; i++) {
+      optsMonths.push({label: paddingLeft(i), value: i})
+    }
+    return optsMonths
+  }
+
+  const optsEducation = [
+    {
+      label: '大专',
+      value: '大专'
+    },
+    {
+      label: '本科',
+      value: '本科'
+    },
+    {
+      label: '硕士',
+      value: '硕士'
+    },
+    {
+      label: '博士',
+      value: '博士'
+    },
+    {
+      label: '其他',
+      value: '其他'
+    }
+  ]
+
+  const optsSeniority = [
+    {
+      label: '应届毕业生',
+      value: 0
+    },
+    {
+      label: '1年',
+      value: 1
+    },
+    {
+      label: '2年',
+      value: 2
+    },
+    {
+      label: '3年',
+      value: 3
+    },
+    {
+      label: '4年',
+      value: 4
+    },
+    {
+      label: '5年',
+      value: 5
+    },
+    {
+      label: '6年',
+      value: 6
+    },
+    {
+      label: '7年',
+      value: 7
+    },
+    {
+      label: '8年',
+      value: 8
+    },
+    {
+      label: '9年',
+      value: 9
+    },
+    {
+      label: '10年',
+      value: 10
+    },
+    {
+      label: '10年以上',
+      value: 100
+    }
+  ]
 
   export default {
     name: 'basic-info',
     components: {
-      selector
+      selector,
+      myRadio,
+      citySelector
     },
     data () {
       return {
@@ -48,7 +171,16 @@
         isShowEditor: false,
         location: '杭州',
         mail: 'mail@mail.com',
+        optsIdentity: [{label: 'XX工程师 · XXXX公司', value: 'XX工程师 · XXXX公司'}, {label: 'XX工程 · XXXX大学', value: 'XX工程 · XXXX大学'}],
+        optsYears: generatorOptsYears(1973, 2001),
+        optsMonths: generatorOptsMonths(),
+        optsEducation,
+        optsSeniority,
         phone: '13X05712017',
+        selectedYearIndex: -1,
+        selectedMonthIndex: -1,
+        selectedEducationIndex: 1,
+        selectedSeniorityIndex: 6,
         seniority: 6,
         sex: 'male'
       }
@@ -62,6 +194,10 @@
 </script>
 
 <style rel="stylesheet/scss" lang="scss">
+.selector_year {
+  margin-right: 10px;
+}
+
 .display-part {
   position: relative;
   width: 604px;
@@ -126,6 +262,9 @@
     color: #afafa9;
     padding-bottom: 8px;
     padding-left: 3px;
+  }
+  .edit-wrap {
+    margin-bottom: 9px;
   }
 }
 </style>
